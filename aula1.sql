1366f8043928bc21dfa0f0921aa325f34223e832
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-02-26 12:03:19 -0300

    aprendendo comandos básicos de sql

---
+++ aula1.sql
@@ -0,0 +1,30 @@
+-- Introdução ao Postgres e comandos básicos de sql.
+
+
+-- O comando SELECT é utilizado para selecionar 1 ou várias colunas de uma tabela.
+SELECT company_name, contact_name FROM customers;
+
+SELECT * FROM products;
+
+SELECT 
+	product_name,
+	quantity_per_unit,
+	unit_price
+FROM products WHERE unit_price > 25;
+
+
+-- O comando AS é utilizado para criar um alias para os nomes das colunas.
+SELECT
+	product_id AS ID_Produto,
+	product_name AS "Nome_Produto"
+FROM products
+
+
+-- O comando LIMIT é utilizado para limitar a quantidade de linhas da tabela.
+SELECT * FROM orders LIMIT 10;
+
+SELECT 
+	customer_id, 
+	ship_city
+FROM orders
+LIMIT 5;
