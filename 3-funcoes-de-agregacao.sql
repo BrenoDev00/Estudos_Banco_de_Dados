4b1f0cad48f5b78b280974f4ccf3c83a9522898a
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-03-02 21:56:37 -0300

    aprendendo a usar o sum

--- 3-funcoes-de-agregacao.sql
+++ 3-funcoes-de-agregacao.sql
@@ -5,3 +5,11 @@
 SELECT COUNT(contact_name) FROM customers;
 
 SELECT COUNT(*) FROM customers; -- asterisco retorna o resultado total de linhas independente de null.
+
+
+-- SUM: Permite somar os valores de determinada coluna.
+SELECT SUM(units_in_stock) FROM products;
+
+SELECT 
+	SUM(quantity) 
+FROM order_details;
