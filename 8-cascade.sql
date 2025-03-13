880784dfe0fb9104de7ce946ac0e3fd982124492
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-03-13 09:44:51 -0300

    adiciona operação INSERT INTO

---
+++ 8-cascade.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO: utilizado para inserir dados em uma tabela.
+INSERT INTO alunos (id_aluno, nome_aluno, email)
+VALUES 
+	(1, 'Breno Silva', 'breno@mail.com'),
+	(2, 'Ana Maria', "ana@mail.com"),
+	(3, 'Carlos', 'carlos@mail.com')
--- hashtag.sql
+++ hashtag.sql
@@ -3,21 +3,49 @@ CREATE DATABASE hashtag;
 
 CREATE TABLE alunos (
 	ID_Aluno INT,
-	Nome_Aluno VARCHAR(100),
-	Email VARCHAR(100)
+	Nome_Aluno VARCHAR(100) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	PRIMARY KEY(ID_Aluno)
 );
 
-CREATE TABLE cursos(
+CREATE TABLE cursos (
 	ID_Curso INT,
-	Nome_Curso VARCHAR(100),
-	Preco_Curso NUMERIC(10, 2)
+	Nome_Curso VARCHAR(100) NOT NULL,
+	Preco_Curso NUMERIC(10,2) NOT NULL,
+	PRIMARY KEY(ID_Curso)
 );
 
 CREATE TABLE matriculas (
-	ID_Matricula INT, 
-	ID_Aluno INT,
-	ID_Curso INT,
-	Data_Cadastro DATE
+	ID_Matricula INT,
+	ID_Aluno INT NOT NULL,
+	ID_Curso INT NOT NULL,
+	Data_Cadastro DATE NOT NULL,
+	PRIMARY KEY(ID_Matricula),
+	FOREIGN KEY(ID_Aluno) REFERENCES alunos(ID_Aluno),
+	FOREIGN KEY(ID_Curso) REFERENCES cursos(ID_Curso)
 );
+
+
+SELECT * FROM cursos;
+
+INSERT INTO alunos (ID_Aluno, Nome_Aluno, Email)
+VALUES 
+	(1, 'Breno', 'breno@mail.com'),
+	(2, 'Pedro Henrique', 'henrique@mail.com'),
+	(3, 'Luana', 'lu@mail.com');
+
+
+INSERT INTO cursos (ID_Curso, Nome_Curso, Preco_Curso)
+VALUES 
+	(1, 'Excel', 60.58),
+	(2, 'Power BI', 74.00),
+	(3, 'Python', 28.99);
+
+
+INSERT INTO matriculas (ID_Matricula, ID_Aluno, ID_Curso, Data_Cadastro)
+VALUES 
+	(1, 3, 2, '2025/02/23'),
+	(2, 1, 1, '2025/03/11'),
+	(3, 2, 3, '2025/01/05');
 
 SELECT * FROM matriculas;
